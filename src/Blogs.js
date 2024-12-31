46f077b465e7c314dcfdaadddd5c68ccd4c150ae
Ashutosh Gangwar <ashutoshgangwar1994@gmail.com>
2025-01-01 01:08:10 +0530

    Bogs component updated in show more and remove extra css

--- src/Blogs.js
+++ src/Blogs.js
@@ -4,14 +4,13 @@ import "./App.css";
 function Blogs() {
   return (
     <div className="maincard">
-      <h2 className="exp">Professional Profile</h2>
+      <h2 className="subheading">"What experts say about games nature"</h2>
       <div className="card-container">
         <div className="card">
-          <div className="image-comapny">
+          <div className="professional-image">
             <img src="neeraj.jpeg" alt="" />
           </div>
-
-          <div className="job-time">
+          <div className="Professional-name">
             <h6
               style={{
                 color: "Black",
@@ -32,36 +31,11 @@ function Blogs() {
             first by any Indian
           </p>
         </div>
+        {/* Repeat the card for other profiles */}
         <div className="card">
           <div className="image-comapny">
             <img src="neeraj.jpeg" alt="" />
           </div>
-
-          <div className="job-time">
-            <h6
-              style={{
-                color: "Black",
-                border: "2px solid Black",
-                borderRadius: "15px",
-                margin: "10px top",
-              }}
-            >
-              <span style={{ margin: "10px" }}>Ashutosh Gangwar</span>{" "}
-            </h6>
-          </div>
-          <p>
-            Despite his young age, Indian javelin thrower Neeraj Chopra has
-            already etched his name in the history books by becoming the first
-            and only track and field athlete from the country to win an Olympic
-            medal that too a gold. He is also a world champion - India's
-            first-ever in senior athletics - and a Diamond League winner, also a
-            first by any Indian
-          </p>
-        </div><div className="card">
-          <div className="image-comapny">
-            <img src="neeraj.jpeg" alt="" />
-          </div>
-
           <div className="job-time">
             <h6
               style={{
@@ -83,11 +57,11 @@ function Blogs() {
             first by any Indian
           </p>
         </div>
+
         <div className="card">
           <div className="image-comapny">
             <img src="neeraj.jpeg" alt="" />
           </div>
-
           <div className="job-time">
             <h6
               style={{
@@ -109,6 +83,36 @@ function Blogs() {
             first by any Indian
           </p>
         </div>
+
+        <div className="card">
+          <div className="image-comapny">
+            <img src="neeraj.jpeg" alt="" />
+          </div>
+          <div className="job-time">
+            <h6
+              style={{
+                color: "Black",
+                border: "2px solid Black",
+                borderRadius: "15px",
+                margin: "10px top",
+              }}
+            >
+              <span style={{ margin: "10px" }}>Ashutosh Gangwar</span>{" "}
+            </h6>
+          </div>
+          <p>
+            Despite his young age, Indian javelin thrower Neeraj Chopra has
+            already etched his name in the history books by becoming the first
+            and only track and field athlete from the country to win an Olympic
+            medal that too a gold. He is also a world champion - India's
+            first-ever in senior athletics - and a Diamond League winner, also a
+            first by any Indian
+          </p>
+        </div>
+      </div>
+      {/* Add "Show More" link */}
+      <div className="show-more">
+        <a href="/more-blogs" className="show-more-link">Show More</a>
       </div>
     </div>
   );
